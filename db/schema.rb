385d3ddf5cfd74219a74e38dc462bd8299832d5c
Mauricio F Martinez <mauriciof.martinez@gmail.com>
2016-05-13 04:28:40 -0700

    Created model for user. With the appropriate fields to handle the logic for password handling. Ran the migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160513111909) do
+ActiveRecord::Schema.define(version: 20160513112719) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 20160513111909) do
     t.text     "body"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
 end
