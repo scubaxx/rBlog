8d64a9dbcf95a84a5f54b4156a518eeab25f14db
Mauricio F Martinez <mauriciof.martinez@gmail.com>
2016-05-13 04:30:08 -0700

    Generate model for comment fields name, email, body Ran the migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160513112719) do
+ActiveRecord::Schema.define(version: 20160513112910) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
