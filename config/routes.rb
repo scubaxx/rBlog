87ad088e0f92d45587c14059cf889286f5d31588
Mauricio F Martinez <mauriciof.martinez@gmail.com>
2016-05-13 03:45:53 -0700

    Created a folder called admin inside the app/controllers folders to host the actions for the admin new, create, edit, update, destroy, index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,32 @@
 Rails.application.routes.draw do
+  namespace :admin do
+  get 'posts/new'
+  end
+
+  namespace :admin do
+  get 'posts/create'
+  end
+
+  namespace :admin do
+  get 'posts/edit'
+  end
+
+  namespace :admin do
+  get 'posts/update'
+  end
+
+  namespace :admin do
+  get 'posts/destroy'
+  end
+
+  namespace :admin do
+  get 'posts/index'
+  end
+
+  namespace :admin do
+  get 'posts/show'
+  end
+
   get 'posts/index'
 
   get 'posts/show'
