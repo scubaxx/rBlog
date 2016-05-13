304207b43e1d733a2b6318fec3e7f63e917fdbd0
Mauricio F Martinez <mauriciof.martinez@gmail.com>
2016-05-13 03:51:47 -0700

    Generated categories controller for the admin in folder admin/categories new create edit update destroy index show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,32 @@
 Rails.application.routes.draw do
+  namespace :admin do
+  get 'categories/new'
+  end
+
+  namespace :admin do
+  get 'categories/create'
+  end
+
+  namespace :admin do
+  get 'categories/edit'
+  end
+
+  namespace :admin do
+  get 'categories/update'
+  end
+
+  namespace :admin do
+  get 'categories/destroy'
+  end
+
+  namespace :admin do
+  get 'categories/index'
+  end
+
+  namespace :admin do
+  get 'categories/show'
+  end
+
   get 'categories/show'
 
   namespace :admin do
