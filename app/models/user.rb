9ed2343da77d3a6bd0c4eb2c3a8b91e1d639d549
Mauricio F Martinez <mauriciof.martinez@gmail.com>
2016-05-21 16:45:40 -0700

    Implement views/admin/posts/edit.html.erb so posts can be edited I just copied the HTML form from views/admin/posts/new.html.erb and changed Add New Post to Edit Post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
 	has_many :posts
+
+	validates :name, presence: true
+	validates :email, presence: true
+	validates :password, presence: true
+	validates :password, confirmation: true
 end
